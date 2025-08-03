05c899d1ab6dcac62c64b0f2e4d8ec8d8b500f32
Emilio Pelaez Cisneros <epelaez@uchicago.edu>
2025-08-03 10:10:03 -0500

    Generate file list for nn data product files

--- GenerateFileList.cc
+++ GenerateFileList.cc
@@ -23,5 +23,23 @@ int main() {
     list_file.close();
     std::cout << "File list saved to " << output_file << std::endl;
     
+    std::string nn_directory_path = "/pnfs/lariat/persistent/users/epelaez/reco_nn_files/";
+    std::string output_nn_file = "nn_files.list";
+
+    std::ofstream nn_list_file(output_nn_file);
+    if (!nn_list_file) {
+        std::cerr << "Error opening NN output file." << std::endl;
+        return 1;
+    }
+
+    for (const auto& entry : fs::directory_iterator(nn_directory_path)) {
+        if (entry.is_regular_file()) {
+            nn_list_file << nn_directory_path + entry.path().filename().string() << std::endl;
+        }
+    }
+
+    nn_list_file.close();
+    std::cout << "NN file list saved to " << output_nn_file << std::endl;
+
     return 0;
 }
