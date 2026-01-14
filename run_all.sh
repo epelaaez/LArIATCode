02d0ca51f5fec3562c83ee5e7b5157a7ac9ee4c5
Emilio Pelaez Cisneros <epelaez@uchicago.edu>
2026-01-13 20:37:00 -0600

    Remove old script

--- run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run each (active) ROOT macro in batch mode (-b).
-
-root -b -q RecoNNShowers.cc
-root -b -q RecoAllAnalysis.cc
-root -b -q RecoClassifyAll.cc
-root -b -q RecoClassifyAllSimplified.cc
-root -b -q RecoDataAnalysis.cc
